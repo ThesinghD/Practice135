836af0715006794c03cee6b7c39083731fac7aa8
Duncan Thesingh <ThesinghD>
2026-01-12 13:32:42 -0500

    added notes doc

---
+++ Notes.py
@@ -0,0 +1,17 @@
+#Terminal:
+#ls = print all items in current directory
+#cd = moves directory
+#pwd = prints current directory
+#mkdir = makes new directory at current locaiton with provided name
+#touch = creates empty file in current directory with given file name
+
+#Git:
+#git add = adds named files to the staging area, ready to be committed
+    #git add. = adds all files in current directory to staging area
+#git commit -m "message" = commits added files with given description
+#git push = pushes all commited files to github, "saving" them online
+#git status = reports current status of directory (changes, new files, etc)
+#git clone (url) = copies github repo onto computer into current directory
+
+#Shortcuts
+#Ctrl+Shift+M = checks code for style mistakes
